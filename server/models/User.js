b824f42116497199414864b0fd307743d97fc303
Festus Ribiro <festusribiro@gmail.com>
2023-07-13 13:19:35 +0300

    Fixed import bug

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('./config/db.Config');
+const sequelize = require('../config/db.config');
 
 const User = sequelize.define("User", {
     UsersId: {
